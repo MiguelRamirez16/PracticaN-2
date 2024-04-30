d208e3b527934a8b75be00064c1c2de79d34e3bb
MIGUEL �NGEL RAM�REZ VEL�SQUEZ <miguelramirez317437@correo.itm.edu.co>
2024-04-29 22:47:29 -0500

    Commit Creacion de la web

--- Solution1/Construction.WEB/Program.cs
+++ Solution1/Construction.WEB/Program.cs
@@ -1,4 +1,6 @@
 using Construction.WEB;
+using Construction.WEB.Repositories;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
+
+builder.Services.AddSweetAlert2();
+
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
---
+++ Solution1/Construction.WEB/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Construction.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+
+
+        Task<HttpResponseWrapper<object>> DeleteAsync(string url);
+
+
+        Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
+        Task<HttpResponseWrapper<TResponse>> PutAsync<T, TResponse>(string url, T model);
+
+
+
+    }
+}
